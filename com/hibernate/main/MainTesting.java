f0e5a21fbeabbc8c89f873eab1bafa4eb2662176
Rahul Shivsharan <rahulshivsharan@gmail.com>
2014-02-20 18:24:12 +0530

    Update MainTesting.java

--- com/hibernate/main/MainTesting.java
+++ com/hibernate/main/MainTesting.java
@@ -6,6 +6,7 @@ package com.hibernate.main;
 
 import com.hibernate.pojo.Customer;
 import com.hibernate.testing.HibernateUtil;
+import java.util.List;
 import org.hibernate.CacheMode;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -69,6 +70,8 @@ public class MainTesting {
         session[2].close();        
     }
 
+    
+    
     private static void loadCustomer() throws Exception {
         Session[] sessions = new Session[]{
             HibernateUtil.getSessionFactory().openSession(),
@@ -83,17 +86,71 @@ public class MainTesting {
         sessions[1].close();
         Customer cus3 = (Customer) sessions[2].load(Customer.class, new Integer(2));
         sessions[2].close();
-        
-        
+                
+    }
+    
+    private static void testCacheOne() throws Exception{
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        List<Customer> list = null;
+        Query query = null;
+        try{
+            query = session.createQuery("from Customer c order by c.customerId desc").setCacheable(Boolean.TRUE).setCacheRegion("cache_two");
+            list  = query.list();
+            
+            for(Customer  c : list){
+                System.out.println(c);
+            }
+            session.beginTransaction();
+            session.save(new Customer(new Integer(21), "Rangarajan Somathne Deorukh" ));
+            session.getTransaction().commit();
+            list  = query.list();
+            
+            for(Customer  c : list){
+                System.out.println(c);
+            }
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            if(session != null && session.isOpen()){
+                session.close();
+            }
+        }
+    }
+    
+    private static void testCacheTwo() throws Exception{
+        Session session = null;
+        Customer c = null;
+        try{
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            c = (Customer) session.get(Customer.class, 21);
+            System.out.println(c);
+            c.setCustomerName("Aslam Zhakir Deshpande");
+            session.merge(c);
+            session.getTransaction().commit();
+            session.close();
+            session = HibernateUtil.getSessionFactory().openSession();
+            c = (Customer) session.get(Customer.class, 21);
+            System.out.println(c);
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            if(session != null && session.isOpen()){
+                session.close();
+            }
+        }
     }
 
     public static void main(String[] args) {
         try {
 //              getCustomers();
 //            getCustomer();
-            getCustomerFromSession();
+//            getCustomerFromSession();
 //            getMyQuery();
 //                loadCustomer();
+//            testCacheOne();
+            testCacheTwo();
         } catch (Exception e) {
             e.printStackTrace();
         }
